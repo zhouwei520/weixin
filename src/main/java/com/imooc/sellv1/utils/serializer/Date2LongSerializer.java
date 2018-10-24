1792a88918ce6fd5649b848e8469482ecb87f7c5
zhouwei520 <zw520ta>
2018-10-24 19:59:46 +0800

    时间工具类

---
+++ src/main/java/com/imooc/sellv1/utils/serializer/Date2LongSerializer.java
@@ -0,0 +1,24 @@
+package com.imooc.sellv1.utils.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * 描述:
+ * 时间转换秒的
+ *
+ * @author mac
+ * @create 2018-10-23 下午9:56
+ */
+public class Date2LongSerializer extends JsonSerializer<Date> {
+
+    @Override
+    public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        jsonGenerator.writeNumber(date.getTime()/1000);
+    }
+}
